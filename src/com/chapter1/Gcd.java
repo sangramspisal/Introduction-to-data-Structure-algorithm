ddf94a20a4953e56aaafc4f40006f968d0a9ee19
sangramspisal <sangram_pisal@yahoo.com>
2016-04-08 22:44:12 -0400

    GCD program Added

---
+++ src/com/chapter1/Gcd.java
@@ -0,0 +1,20 @@
+package com.chapter1;
+
+public class Gcd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=50;
+		System.out.println("Gcd of two number is:");
+		System.out.println(new Gcd().gcdFinder(a,b));
+
+	}
+	public int gcdFinder(int a,int b){
+		if(b==0){
+			return a;
+		}
+		return gcdFinder(a, a%b);
+		}
+
+}
